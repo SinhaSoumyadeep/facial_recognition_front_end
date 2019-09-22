b0f76ae879a3799e65074332a13cf5ff16892789
ssinha <ssinha@Veracode.com>
2019-09-22 18:14:09 -0400

    matching faces

--- src/Service/FaceService.js
+++ src/Service/FaceService.js
@@ -1,5 +1,6 @@
-
-const addUrl = "http://127.0.0.1:8000/blog/"
+import axios from 'axios'
+const addUrl = "http://127.0.0.1:8000/blog/match/"
+const uploadUrl = "http://127.0.0.1:8000/blog/upload/"
 
 let _singleton = Symbol();
 
@@ -22,6 +23,24 @@ class FaceService {
             method: 'POST'
         }).then(function (response) {
             console.log(response)
+            return response.json();
+        })
+    }
+
+    upload(selectedFile) {
+        const fd = new FormData();
+        fd.append('image', selectedFile,  selectedFile.name)
+        return axios.post(uploadUrl, fd).then(res => { console.log(res); return res.data})
+    }
+
+    loadFaces() {
+        return fetch(addUrl, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'GET'
+        }).then(function (response) {
+            console.log(response);
             return response.json();
         })
     }
