bb964f3bb02a797070d4445ef983ef66604cea53
ssinha <ssinha@Veracode.com>
2019-09-23 14:50:03 -0400

    adding css and bootstrap

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import FaceService from "../Service/FaceService";
+import '../CSS/jazz.css'
+import slytherin from '../Images/slytherin.gif'
 
 
-export default class Home extends React.Component
-{
+export default class Home extends React.Component {
 
     constructor() {
         super();
@@ -11,9 +12,8 @@ export default class Home extends React.Component
         this.upload = this.upload.bind(this);
         this.fs = FaceService.instance;
         this.state = {
-            match: "hrithik.jpeg",
-            face: null
-
+            match: slytherin,
+            face: null,
         };
     }
 
@@ -23,21 +23,29 @@ export default class Home extends React.Component
 
     }
 
-    loadCelebrityEncodings(){
-        this.fs.loadFaces().then((res)=>{console.log(res)})
+    loadCelebrityEncodings() {
+        this.fs.loadFaces().then((res) => {
+            console.log(res)
+        })
 
     }
 
     createModule(res) {
 
-        this.fs.createFaces(res).then((res)=>{this.setState({ match:  res.url , dist: res.distance})})
+        this.fs.createFaces(res).then((res) => {
+            this.setState({
+                match: "https://facial-rec-1992-known-bucket.s3.amazonaws.com/" + res.url,
+                dist: res.distance
+            })
+        })
 
     }
 
     upload() {
 
-        this.fs.upload(this.state.face).then((res)=>{this.createModule(res) })
-
+        this.fs.upload(this.state.face).then((res) => {
+            this.createModule(res)
+        })
 
 
     }
@@ -45,25 +53,45 @@ export default class Home extends React.Component
     fileSelectedHandler = event => {
         this.setState({
             face: event.target.files[0],
-            preview: URL.createObjectURL(event.target.files[0])
+            match: URL.createObjectURL(event.target.files[0])
         })
         console.log(event.target.files[0])
     }
 
-    render(){
+    render() {
+        let image;
+        if (this.state.match != null) {
+            image = <div className={"card"} style={{width: "18rem"}}>
+                <img className={"card-img-top"} src={this.state.match} style={{height: "300px", width: "286px"}}/>
+                {/*<div className={"card-body"}>*/}
+                {/*    <p className={"card-text"}>{this.state.dist}</p>*/}
+                {/*</div>*/}
+            </div>
+        } else {
 
-        return(
+        }
+
+
+        return (
 
             <div>
-                <h1> Celebrity Twin</h1>
-                <input type="file" onChange={this.fileSelectedHandler}/>
-                <button onClick={this.upload}>
-                    upload
-                </button>
-                <h1>{this.state.dist}</h1>
-                <img src={this.state.preview} style={{height: "300px", width: "300px"}}/>
-                &nbsp;&nbsp;&nbsp;
-                <img src={"https://facial-rec-1992-known-bucket.s3.amazonaws.com/"+this.state.match} style={{height: "300px", width: "300px"}}/>
+                <div className={"container-fluid"}>
+                    <h1> Celebrity Twin</h1>
+                    <div className={"mirror"}>
+                        {image}
+                        <input style={{display: 'none'}} type="file" onChange={this.fileSelectedHandler}
+                               ref={fileInput => this.fileInput = fileInput}/>
+                        <div className={"buttonPicker"}>
+                            <button style={{width: "137px"}} type="button" className="btn btn-success"
+                                    onClick={() => this.fileInput.click()}>Pick
+                            </button>
+                            &nbsp;&nbsp;&nbsp;
+                            <button style={{width: "137px"}} type="button" className="btn btn-success"
+                                    onClick={this.upload}>Upload
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
 
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Dashboard from "./Containers/Dashboard";
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import bg from '../src/Images/background.jpg'
 ReactDOM.render(
     <div className="container-fluid">
         <Dashboard/>
